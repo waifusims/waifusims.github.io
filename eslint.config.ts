b9fa9508709f9b92c62af0e96c019dffa46a6a3f
stapler <psidefenseup@gmail.com>
2025-02-21 15:48:35 -0600

    configure paths in tsconfig, add @limegrass/eslint-plugin-import-alias

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,16 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import pluginImportAlias from "@limegrass/eslint-plugin-import-alias";
 
 const config = tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   {
+    plugins: {
+      ["@limegrass/import-alias"]: pluginImportAlias,
+    },
     rules: {
-      "@typescript-eslint/prefer-readonly-parameter-types": "error"
+      "@typescript-eslint/prefer-readonly-parameter-types": "error",
+      "@limegrass/import-alias/import-alias": ["error"],
     },
   },
   {
