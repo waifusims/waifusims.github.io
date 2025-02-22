7d6c4fbef68d3ab73685ab41771d1d189285bc28
stapler <psidefenseup@gmail.com>
2025-02-21 23:32:39 -0600

    again again

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,30 @@
 "use client"
 
-import { redirect, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const CASE_INSENSITIVE_ROUTES = new Set([
 	"/eov/skillsim.html",
 ]);
 
 export default function NotFoundPage() {
+	const router = useRouter();
+	const [isNotFound, setIsNotFound] = useState(false);
 	const pathname = usePathname();
-	const lowerPath = pathname.toLowerCase();
-	const shouldRedirect = CASE_INSENSITIVE_ROUTES.has(lowerPath);
-	if (shouldRedirect) {
-		redirect(lowerPath);
+	useEffect(() => {
+		const lowerPath = pathname.toLowerCase();
+		const shouldRedirect = CASE_INSENSITIVE_ROUTES.has(lowerPath);
+		if (shouldRedirect) {
+			void router.replace(lowerPath);
+		} else {
+			setIsNotFound(true);
+		}
+	}, [pathname, router]);
+
+	if (!isNotFound) {
+		return null;
 	}
 
-	return shouldRedirect ? null : <h1>404 - Page Not Found</h1>
+	return <h1>404 - Page Not Found</h1>
 }
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,20 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginImportAlias from "@limegrass/eslint-plugin-import-alias";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from 'eslint-plugin-react-hooks';
 
 const config = tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  pluginReactHooks.configs["recommended-latest"],
   {
+    settings: {
+      react: {
+        version: "detect",
+      }
+    },
     plugins: {
       ["@limegrass/import-alias"]: pluginImportAlias,
     },
