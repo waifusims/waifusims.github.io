d2b45ae61d55375cf0c7b4b3493e9531d4677618
stapler <psidefenseup@gmail.com>
2025-02-21 20:07:16 -0600

    fix old links to eov skillsim

--- app/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-export function middleware(request: NextRequest) {
-	return NextResponse.rewrite(new URL(request.nextUrl.pathname.toLocaleLowerCase(), request.url));
-}
---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { redirect, usePathname } from "next/navigation";
+
+const CASE_INSENSITIVE_ROUTES = new Set([
+	"/eov/skillsim.html",
+]);
+
+export default function NotFoundPage() {
+	const pathname = usePathname();
+	const lowerPath = pathname.toLowerCase();
+	if (CASE_INSENSITIVE_ROUTES.has(lowerPath)) {
+		redirect(lowerPath);
+	}
+
+	return <h1>404 - Page Not Found</h1>
+}
